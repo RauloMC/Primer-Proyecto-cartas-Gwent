98eba2acdf4edf9f3d22419dbd53b0b21a2694db
Raulo <rauloprogramador@gmail.com>
2024-03-31 12:11:54 -0400

    Modified new script

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isDragging = false;
+    public bool isOverDropZone;
+    public GameObject meleeZone;
+     public Vector2 startPosition;
+    void Update()    
     {
+        if(isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y); 
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDragging()
     {
-        
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDragging()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(meleeZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+      private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        meleeZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        meleeZone = null;
     }
 }
